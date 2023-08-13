93905bc68b32490b2c257537554869aaad13e5c8
Christian Limtiaco <climtiaco.code@gmail.com>
2023-08-13 15:36:36 -0700

    Finished assignment

--- public/index.js
+++ public/index.js
@@ -1,9 +1,17 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    const refreshButton = document.querySelector('#refresh');
 
     let countValue = 0;
+
+    const response = await fetch('http://localhost:9001/counter');
+    const data = await response.json();
+
+    console.log(data)
+
+    countValue = data.value;
 
     function increment(){
         countValue++;
@@ -15,8 +23,14 @@ function main(){
         countContainer.textContent = countValue;
     }
 
+    function refresh(){
+        countValue = 0;
+        countContainer.textContent = countValue;
+    }
+
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    refreshButton.addEventListener('click', refresh)
     countContainer.textContent = countValue;
 }
 main()
